2e520036168cca8c7a832950a25ebe10751cd6d9
Anna Chumakova <chumakova3008@gmail.com>
2020-04-28 01:16:16 +0500

    Added test

---
+++ src/js/__tests__/basic.test.js
@@ -0,0 +1,30 @@
+import getPersonsHealthList from '../basic';
+
+describe('getPersonsHealthList', () => {
+  test.each([
+    [
+      'empty arr',
+      [],
+      [],
+    ],
+    [
+      'identical objects',
+      [{ name: 'Mag', health: 10 }, { name: 'Thief', health: 10 }, { name: 'Warrior', health: 10 }],
+      [{ name: 'Warrior', health: 10 }, { name: 'Thief', health: 10 }, { name: 'Mag', health: 10 }],
+    ],
+    [
+      'different objects',
+      [{ name: 'Mag', health: 10 }, { name: 'Thief', health: 50 }, { name: 'Warrior', health: 80 }],
+      [{ name: 'Warrior', health: 80 }, { name: 'Thief', health: 50 }, { name: 'Mag', health: 10 }],
+    ],
+    [
+      'different objects',
+      [{ name: 'Warrior', health: 80 }, { name: 'Mag', health: 10 }, { name: 'Thief', health: 50 }],
+      [{ name: 'Warrior', health: 80 }, { name: 'Thief', health: 50 }, { name: 'Mag', health: 10 }],
+    ],
+  ])('%s', (name, persons, expected) => {
+    const result = getPersonsHealthList(persons);
+
+    expect(result).toEqual(expected);
+  });
+});
